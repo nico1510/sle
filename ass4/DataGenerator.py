b18adc25eca193e978b386fe602b45eac5782949
Nicolas Beck <nico@Nico-ubuntu.(none)>
2014-01-30 12:22:26 +0100

    refact

--- ass4/DataGenerator.py
+++ ass4/DataGenerator.py
@@ -11,25 +11,25 @@ def generateTemplate(filename):
     noStates = 0
     currentState = None
     currentTransition = None
-    edgeList = []
+    transList = []
 
     for index, token in enumerate(content):
         if "#initState#" in token:
             content[index] = "{{ states[0].name }}"
             currentState = 0
             currentTransition = -1
-            edgeList.insert(0,0)
+            transList.insert(0,0)
 
         if "#stateDecl#" in token:
             noStates += 1
             currentState = noStates
             content[index] = "{{ states[" + str(currentState) + "].name }}"
             currentTransition = -1
-            edgeList.append(0)
+            transList.append(0)
 
         if "#input#" in token:
             currentTransition += 1
-            edgeList[currentState]=currentTransition+1
+            transList[currentState]=currentTransition+1
             content[index] = "{{ states[" + str(currentState) + "].transitions[" + str(currentTransition) + "].input }}"
 
         if "#action#" in token:
@@ -41,7 +41,7 @@ def generateTemplate(filename):
     templatefile = open(filename, 'w')
     templatefile.write(' '.join(content))
 
-    return edgeList
+    return transList
 
 def generateTestFiles(depth):
     oldFiles = glob.glob('./templates/template*')+glob.glob('./testdata/*.fsml')
@@ -54,10 +54,10 @@ def generateTestFiles(depth):
 
     for file in templatefiles:
         # after this command, the placeholders of the file are replaced with jinja placeholders
-        edgeList = generateTemplate(file)
+        transList = generateTemplate(file)
 
         try:
-            fsm = createSpecificFSM(edgeList)
+            fsm = createSpecificFSM(transList)
             template = env.get_template(file)
             testdata = template.render(states=fsm)
             testdataFile = open(os.path.join("./testdata", "sample"+file.split("template")[2]+".fsml"), 'w')
