66d5771b34d3c1e7d2cd0bc9cbc8f2dd0399dfc3
Nicolas Beck <nico@Nico-ubuntu.(none)>
2014-01-30 15:28:44 +0100

    Testsuite used

--- ass4/TestModule.py
+++ ass4/TestModule.py
@@ -1,5 +1,6 @@
 import glob
 import sys
+import unittest
 
 sys.path.append("../ass2")
 
@@ -8,12 +9,25 @@ from ConstraintChecker import ok
 from FsmExceptions import *
 from DataGenerator import generateTestFiles
 
-generateTestFiles(7)
-testFiles = glob.glob('./testdata/*.fsml')
 
-for f in testFiles:
-    try:
-        fsm = parseFSM(f)
-        ok(fsm)
-    except FsmException:
-        raise
+class DataTestCase(unittest.TestCase):
+    def __init__(self, testdataFile):
+        unittest.TestCase.__init__(self, methodName='testOneFile')
+        self.testdataFile = testdataFile
+
+    def testOneFile(self):
+        fsm = parseFSM(self.testdataFile)
+        self.assertTrue(ok(fsm))
+
+    def shortDescription(self):
+        return 'TestCase for file %s' % self.testdataFile
+
+
+def get_test_data_suite():
+    generateTestFiles(8)
+    testFiles = glob.glob('./testdata/*.fsml')
+    return unittest.TestSuite([DataTestCase(testFile) for testFile in testFiles])
+
+if __name__ == '__main__':
+    testRunner = unittest.TextTestRunner()
+    testRunner.run(get_test_data_suite())
